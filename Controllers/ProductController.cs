eaa9497576f5c1afc14b8a8043f6f7afedc7a531
Nikhil Rathod <128123668+itnikhilratho21-a11y@users.noreply.github.com>
2026-09-26 15:10:59 +0530

    Add NewController

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,66 @@
+﻿using PaasData.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PaasData.Controllers
+{
+    public class ProductController : Controller
+    {
+       
+     public ActionResult Index()
+     {
+            List<Product> products = new List<Product>()
+            {
+                new Product(){Id=1 ,Name="Jens", Price=999},
+                new Product(){Id=2,Name="AmzonShirt",Price=2999},
+                new Product(){Id=3,Name="JsShirt",Price=400 }
+
+            };
+
+            TempData["products"] = products;
+
+            return View(products);
+     }
+        public ActionResult Create()
+        {
+            return View();
+
+        }
+
+        [HttpGet]
+        public ActionResult Edit( int? id)
+        {
+            List<Product> products = (List<Product>)TempData.Peek("products");
+
+            Product product = products.FirstOrDefault(p=>p.Id ==id);
+            return View(product);
+                
+        }
+        [HttpPost]
+        public ActionResult Edit(int? id,Product updateProduct)
+        {
+            List<Product> products = (List<Product>)TempData.Peek("products");
+            Product product = products.FirstOrDefault(p => p.Id == id);
+            return View(product);
+
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int? id)
+        {
+            List<Product> products = (List<Product>)TempData.Peek("products");
+            Product product = products.FirstOrDefault(p => p.Id == id);
+            return View(product);
+        }
+
+        
+
+
+
+
+
+    }
+}
---
+++ Helpers/CustomHtmlHelpers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PaasData.Helpers
+{
+    public static class CustomHtmlHelpers
+    {
+        public static MvcHtmlString Button(
+            this HtmlHelper helper,
+            string type,
+            string value)
+        {
+            TagBuilder tb = new TagBuilder("input");
+
+            tb.Attributes.Add("type", type);
+            tb.Attributes.Add("value", value);
+
+            return new MvcHtmlString(tb.ToString());
+        }
+    }
+}
